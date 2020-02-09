67a9d6b3b8c8000abee9a47375f5c75f3443bffe
gellirosario <gellirosario@gmail.com>
2020-02-09 22:48:35 +0800

    Minor edit

--- src/components/views/Main.js
+++ src/components/views/Main.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Image, Button } from 'react-bootstrap';
 
 function MainPage() {
   return (
 
     <div>
       <div>
-        <img src="https://cdn.psychologytoday.com/sites/default/files/field_blog_entry_images/2017-12/dementia_istock_000029744938_large.jpg" class="img-fluid" alt="Responsive image" fluid></img>
+        <img src="https://cdn.psychologytoday.com/sites/default/files/field_blog_entry_images/2017-12/dementia_istock_000029744938_large.jpg" class="img-fluid" alt="Header"></img>
       </div>
+      <br></br>
+      <p class="h1">Featured Games</p>
       <div class="card-deck" style={{ marginTop: "20px", marginBottom: "20px" }}>
         <div class="card">
-          <img class="card-img-top" style={{height:"53%"}} src="https://store-images.s-microsoft.com/image/apps.54588.14444766800945903.ce1add5c-079a-4ada-ad7e-3edf619f02e2.e0d588f2-ce79-41f6-abd6-a00672d9952f?mode=scale&q=90&h=1080&w=1920"></img>
+          <img class="card-img-top" style={{ height: "53%" }} src="https://store-images.s-microsoft.com/image/apps.54588.14444766800945903.ce1add5c-079a-4ada-ad7e-3edf619f02e2.e0d588f2-ce79-41f6-abd6-a00672d9952f?mode=scale&q=90&h=1080&w=1920" alt="Card Match"></img>
           <div class="card-body">
             <h5 class="card-title">Card Match</h5>
             <p class="card-text">This is a wider card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This card has even longer content than the first to show that equal height action.</p>
@@ -18,7 +19,7 @@ function MainPage() {
           </div>
         </div>
         <div class="card">
-          <img class="card-img-top" style={{height:"53%"}} src="https://www.skincancer.org/wp-content/uploads/whackamole-900px.jpg"></img>
+          <img class="card-img-top" style={{ height: "53%" }} src="https://www.skincancer.org/wp-content/uploads/whackamole-900px.jpg" alt="Whack A Mole"></img>
           <div class="card-body">
             <h5 class="card-title">Whack A Mole</h5>
             <p class="card-text">This is a wider card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This card has even longer content than the first to show that equal height action.</p>
@@ -26,7 +27,7 @@ function MainPage() {
           </div>
         </div>
         <div class="card">
-          <img class="card-img-top" style={{height:"53%"}} src="https://www.teachstarter.com/wp-content/uploads/2019/04/preview-2732014-534438-0-landscape-1200x628.png"></img>
+          <img class="card-img-top" style={{ height: "53%" }} src="https://www.teachstarter.com/wp-content/uploads/2019/04/preview-2732014-534438-0-landscape-1200x628.png" alt="Simon Says"></img>
           <div class="card-body">
             <h5 class="card-title">Simon Says</h5>
             <p class="card-text">This is a wider card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This card has even longer content than the first to show that equal height action.</p>
