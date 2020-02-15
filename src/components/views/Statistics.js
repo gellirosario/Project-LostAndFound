703c961dfa34adce64cc31df7bd92ff337fe5f38
gellirosario <gellirosario@gmail.com>
2020-02-16 04:02:28 +0800

    Added sample charts page and minor update css due to clashing names

---
+++ src/components/views/Statistics.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Bar, Doughnut, Line, Pie, Polar, Radar } from 'react-chartjs-2';
+import { Card, CardBody, CardColumns, CardHeader } from 'reactstrap';
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+
+const BarGraph = React.lazy(() => import('../graphs/BarGraph'));
+
+class Statistics extends Component {
+    constructor() {
+        super();
+        this.state = {
+            chartData: {}
+        }
+    }
+
+    componentWillMount() {
+        this.getChartData();
+    }
+
+    getChartData() {
+        // Ajax calls here
+        this.setState({
+            chartData: {
+                labels: ['Card Match', 'Whack A Mole', 'Simon Says'],
+                datasets: [
+                    {
+                        label: 'No. of Plays',
+                        data: [
+                            34,
+                            45,
+                            60,
+                        ],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(255, 206, 86, 0.6)',
+                        ],
+                        borderWidth: 1,
+                        hoverBackgroundColor: [
+                            'rgba(255, 99, 132, 0.3)',
+                            'rgba(54, 162, 235, 0.3)',
+                            'rgba(255, 206, 86, 0.3)',
+                        ],
+                        hoverBorderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                        ],
+                    }
+                ]
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="animated fadeIn">
+                <CardColumns className="cols-2">
+                    <Card>
+                        <CardHeader>
+                            Top Games
+                        </CardHeader>
+                        <CardBody>
+                            <div className="chart-wrapper">
+                                <BarGraph  chartData={this.state.chartData} legendPosition="bottom" />
+                            </div>
+                        </CardBody>
+                    </Card>
+                </CardColumns>
+            </div>
+        );
+    }
+}
+
+export default Statistics;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const MainPage = React.lazy(() => import('./components/views/Main'));
 const MoleGame = React.lazy(() => import('./components/mole-game/MoleGame'));
 const MatchGame = React.lazy(() => import('./components/match-game/MatchGame'));
 const SimonGame = React.lazy(() => import('./components/simonsays-game/SimonGame'));
+const Statistics = React.lazy(() => import('./components/views/Statistics'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' , component: MainPage},
@@ -11,6 +12,7 @@ const routes = [
   { path: '/game/molegame', name: 'Whack A Mole', component: MoleGame },
   { path: '/game/matchgame', name: 'Card Match', component: MatchGame },
   { path: '/game/simongame', name: 'Simon Says', component: SimonGame},
+  { path: '/statistics', name: 'Statistics', component: Statistics},
 ];
 
 export default routes;
