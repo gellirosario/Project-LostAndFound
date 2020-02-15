229561e111056c0cb9127b82850f1e30606c59bf
gellirosario <gellirosario@gmail.com>
2020-02-16 04:18:12 +0800

    Changed conflicting class name

--- src/components/simonsays-game/SimonGame.js
+++ src/components/simonsays-game/SimonGame.js
@@ -12,12 +12,12 @@ class SimonGame extends React.Component {
 	 <div className="mole_background">
 		<h1 class="display-1" className="mole_title" >SIMON SAYS</h1>
 			<div class="column">
-				<div type="button" id="green" className="btn green"></div>
-				<div type="button" id="red" className="btn red"></div>
+				<div type="button" id="green" className="btn2 green"></div>
+				<div type="button" id="red" className="btn2 red"></div>
 			</div>
 			<div class="column">
-				<div type="button" id="yellow" className="btn yellow"></div>
-				<div type="button" id="blue" className="btn blue"></div>
+				<div type="button" id="yellow" className="btn2 yellow"></div>
+				<div type="button" id="blue" className="btn2 blue"></div>
 			</div>
 			 <h2 type="button"className='start' onClick={() => this.handleStart()}>Start/Restart </h2>
 			<h2 id="score">Score: 0</h2>
