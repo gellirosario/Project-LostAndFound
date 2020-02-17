c8244101fde561d8ed25105cb650ed4186405e72
Edmund <edmund_a@hotmail.com>
2020-02-17 15:11:50 +0800

    no message

--- src/components/simonsays-game/SimonGame.js
+++ src/components/simonsays-game/SimonGame.js
@@ -3,28 +3,29 @@ import React from 'react';
 
 class SimonGame extends React.Component {
  
-
-
-
-
   render() {
     return (
 	 <div className="mole_background">
-		<h1 class="display-1" className="game_title" >SIMON SAYS</h1>
+		<h1 class="display-1" className="mole_title" >SIMON SAYS</h1>
 			<div class="column">
-				<div type="button" id="green" className="btn2 green"></div>
-				<div type="button" id="red" className="btn2 red"></div>
+				<div type="button" id="green" className="btn_ green"></div>
+				<div type="button" id="red" className="btn_ red"></div>
 			</div>
 			<div class="column">
-				<div type="button" id="yellow" className="btn2 yellow"></div>
-				<div type="button" id="blue" className="btn2 blue"></div>
+				<div type="button" id="yellow" className="btn_ yellow"></div>
+				<div type="button" id="blue" className="btn_ blue"></div>
 			</div>
-			 <h2 type="button"className='start' onClick={() => this.handleStart()}>Start/Restart </h2>
+			 <button type="button"className='start_button orange' onClick={() => this.handleStart()}>Start/Restart </button>
 			<h2 id="score">Score: 0</h2>
 
 	  </div>
     );
   }
+
+
+
+
+
 }
 
 
