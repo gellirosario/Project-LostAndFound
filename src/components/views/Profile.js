1d5c3b90cd2f8d7d3f9c994019626a17c3282c8f
mseetbh <mseetbh@gmail.com>
2020-03-07 00:53:50 +0800

    Profile WIP (combine view and edit)

--- src/components/views/Profile.js
+++ src/components/views/Profile.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Card,
   CardBody,
   CardTitle,
   Col,
   Row,
-} from 'reactstrap';
+  Container,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
 //import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import axios from "axios";
 
 class Profile extends Component {
   constructor() {
     super();
     this.state = {
-    }
+      user: [],
+      edit: false
+    };
   }
 
+  componentDidMount() {
+    console.log(this.props.auth.user.id);
+
+    axios.get("/users/" + this.props.auth.user.id).then(res => {
+      this.setState({ user: res.data });
+      console.log(this.state.user);
+    });
+  }
+
+  editProfile() {
+    this.setState({
+      edit: !this.state.edit
+    });
+  }
 
   render() {
     return (
@@ -28,10 +50,20 @@ class Profile extends Component {
                   <Col>
                     <Row>
                       <Col>
-                        <CardTitle className="h3" style={{ paddingTop: 10 }}>View Profile</CardTitle>
+                        <CardTitle className="h3" style={{ paddingTop: 10 }}>
+                          View Profile
+                        </CardTitle>
                       </Col>
                       <Col sm="1.2" style={{ marginRight: 20 }}>
-                        <button type="button" className="start_button orange">Edit Profile</button>
+                        <button
+                          type="button"
+                          className="start_button orange"
+                          onClick={this.editProfile.bind(this)}
+                        >
+                          {this.state.edit
+                            ? "Stop Editing Profile"
+                            : "Edit Profile"}
+                        </button>
                       </Col>
                     </Row>
                     <hr />
@@ -39,14 +71,137 @@ class Profile extends Component {
                       <Col>
                         <Card>
                           <CardBody>
-                            <h4> PROFILE DATA here <br /><br /><br /><br /><br /><br /><br /><br /></h4>
+                            <Form>
+                              <FormGroup>
+                                <Label for="username">
+                                  <h6>Username</h6>
+                                </Label>
+                                <Input
+                                  type="username"
+                                  name="username"
+                                  id="username"
+                                  defaultValue={this.state.user.name}
+                                  disabled={!this.state.edit}
+                                />
+                              </FormGroup>
+                              <FormGroup>
+                                <Label for="email">
+                                  <h6>Email</h6>
+                                </Label>
+                                <Input
+                                  type="email"
+                                  name="email"
+                                  id="email"
+                                  defaultValue={this.state.user.email}
+                                  disabled={!this.state.edit}
+                                />
+                              </FormGroup>
+                              <FormGroup>
+                                <Label for="gender">
+                                  <h6>Gender</h6>
+                                </Label>
+                                <Input
+                                  type="gender"
+                                  name="gender"
+                                  id="gender"
+                                  placeholder={this.state.user.gender}
+                                  disabled
+                                />
+                              </FormGroup>
+                              <FormGroup>
+                                <Label for="gender">
+                                  <h6>Age</h6>
+                                </Label>
+                                <Input
+                                  type="password"
+                                  name="password"
+                                  id="password"
+                                  placeholder={this.state.user.age}
+                                  disabled
+                                />
+                              </FormGroup>
+                              <FormGroup>
+                                <Label for="password">
+                                  <h6>Password</h6>
+                                </Label>
+                                <Input
+                                  type="password"
+                                  name="password"
+                                  id="password"
+                                  placeholder={
+                                    this.state.edit
+                                      ? "Type in a new password"
+                                      : "******"
+                                  }
+                                  disabled={!this.state.edit}
+                                />
+                              </FormGroup>
+                              <FormGroup
+                                style={
+                                  this.state.edit ? {} : { display: "none" }
+                                }
+                              >
+                                <Label for="confirmPassword">
+                                  <h6>Confirm Password</h6>
+                                </Label>
+                                <Input
+                                  type="password"
+                                  name="password"
+                                  id="confirmPassword"
+                                  placeholder="Repeat the same password"
+                                />
+                              </FormGroup>
+                              <br></br>
+                              <button
+                                type="button"
+                                className="start_button orange"
+                                style={
+                                  this.state.edit ? {} : { display: "none" }
+                                }
+                              >
+                                Update Information
+                              </button>
+                            </Form>
+                            {/* <Row>
+                              <Col>
+                                <h5>Username</h5>
+                              </Col>
+                            </Row>
+                            <Row>
+                                {this.state.user.name}
+                              </Row>
+                            <br/>
+                            <Row>
+                              <Col>
+                                <h5>Email</h5>
+                              </Col>
+                              <Col>
+                              {this.state.user.email}
+                              </Col>
+                            </Row>
+                            <br/>
+                            <Row>
+                              <Col>
+                                <h5>Gender</h5>
+                              </Col>
+                              <Col>
+                              {this.state.user.gender}
+                              </Col>
+                            </Row>
+                            <br/>
+                            <Row>
+                              <Col>
+                                <h5>Age</h5>
+                              </Col>
+                              <Col>
+                              </Col>
+                            </Row> */}
                           </CardBody>
                         </Card>
                       </Col>
-                      
                     </Row>
                     <Row>
-                    <Col>
+                      <Col>
                         <h4>History</h4>
                         <hr />
                       </Col>
@@ -66,6 +221,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(
-  mapStateToProps
-)(Profile);
+export default connect(mapStateToProps)(Profile);
