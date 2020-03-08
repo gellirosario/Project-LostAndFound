989ac1b61873bf2b469e630054aa88bb8ea3e53f
gellirosario <gellirosario@gmail.com>
2020-03-08 16:13:06 +0800

    Chat WIP - Updated date/time

--- src/components/chat/MessageList.js
+++ src/components/chat/MessageList.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import Moment from 'react-moment';
 
 class MessageList extends Component {
-
-    constructor() {
-        super();
-
-        var today = new Date(),
-            date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + " [" + today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds() + "]";
-
-        this.state = {
-            date: date
-        };
-    }
 
     render() {
         return (
             <ul className="message-list">
                 {this.props.messages.map((message, index) => (
                     <li className="message-box" key={index}>
-                        <h4 className="message-sender">{message.senderId} @ {this.state.date} </h4>
+                        <h4 className="message-sender">{message.senderId} @ <Moment
+                            calendar={{
+                                lastDay: '[Yesterday at] LT',
+                                sameDay: '[Today at] LT',
+                                nextDay: '[Tomorrow at] LT',
+                                lastWeek: '[last] dddd [at] LT',
+                                nextWeek: 'dddd [at] LT',
+                                sameElse: 'L',
+                                lastDay: '[Yesterday at] LT',
+                                lastWeek: '[last] dddd [at] LT',
+                            }}
+                        >
+                            {message.createdAt}
+                        </Moment>
+                        </h4>
                         <p className="message-text">{message.text}</p>
                     </li>
                 ))}
